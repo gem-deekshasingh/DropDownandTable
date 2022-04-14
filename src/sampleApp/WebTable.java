dbb9308b6abb495d209b85935c4fe14b9d018cad
de.singh2 <Deeksha.Singh@Geminisolutions.com>
2022-04-14 15:54:52 +0530

    k

--- src/sampleApp/WebTable.java
+++ src/sampleApp/WebTable.java
@@ -4,20 +4,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class WebTable {
     public static void main(String[] args) {
         System.setProperty("webdriver.edge.driver","C:\\Users\\de.singh2\\Downloads\\edgedriver_win64\\msedgedriver.exe");
         WebDriver driver = new EdgeDriver();
-        driver.get("file:///C:/Users/de.singh2/Downloads/Assignment.html");
 
-        WebElement mytable=driver.findElement(By.xpath("/html/body/table/tbody"));
-        List<WebElement> rows_table = mytable.findElements(By.tagName("tr"));
 
-        int rows_count = rows_table.size();
+        String baseUrl = "//C:/Users/de.singh2/Downloads/Assignment.html";
+        driver.get(baseUrl);
 
-        System.out.println(rows_count );
+        List<WebElement> rows = driver.findElements(By.xpath("//tbody/tr"));
+        rows.remove(0);
+
+        LinkedHashSet<String> unique = new LinkedHashSet<>();
+
+        for(int i=0;i< rows.size();i++) {
+            unique.add(rows.get(i).getText());
+        }
+
+        System.out.println("Unique rows in table: ");
+        for (String data:unique) {
+            System.out.println(data);
+        }
+
+
+
 
     }
 }
